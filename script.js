884303876ba8872190f231ff92fb0fd425520085
llaulier <lioraloche@yahoo.fr>
2020-03-19 11:34:04 +0100

    js try 1 trust us section

--- script.js
+++ script.js
@@ -38,8 +38,22 @@
 // THEY TRUST US //
 ///////////////////
 
+// class
+const containerTestimonial = document.getElementsByClassName('container-testimonial');
+const testimonial = document.getElementsByClassName('testimonial');
+
+//Buttons
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
+
+//counter
+let counter = 1;
+const testimonialSlide = testimonial[0]
+
+//Button Listerners
+rightBtn.addEventListener('click', () => {
+    testimonial.style.display ='initial';
+});
 
 
 //////////////
