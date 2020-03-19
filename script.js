336e8fc227cf081357f12b69ad9e735cdc7b3117
yann dubois <yanndubois4@gmail.com>
2020-03-19 09:22:12 +0100

    editing some changes in script.js et index.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 ///////////////
-// SLIDESHOW //
-//////////////
+// FOOTER //
+
+const footerElem = document.getElementById('aboutUs');
+
+function changeStyler(newColor,size,content) {
+    footerElem.style.color = newColor;
+    footerElem.style.fontSize = size;
+    footerElem.innerHTML = content;
+  };
+  
+
+  footerElem.addEventListener("mouseover", changeStyler('blue','2 rem', 'discover more !' ));
