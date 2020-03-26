fe8b97eb5cb1a178cbd4bba2dd6005e354c0c14d
yann <yanndubois4@gmail.com>
2020-03-26 10:24:33 +0100

    making the cookies baner sticky

--- script.js
+++ script.js
@@ -3,38 +3,38 @@
 
 const main = document.getElementById('main');
 const cookies = document.getElementById('cookies');
-const buttonElement = document.getElementById('btn');
+const buttonElement = document.getElementsByClassName('btn');
 const trigger1 = 'load'; 
- const trigger2 = 'click'; 
+ const trigger2 = 'click';
 
 
 console.log(cookies);
 
-function bluredBody (filter) {
+ bluredBody = (filter) => {
     main.style.filter = filter;
 
 
 };
 
 
-function acceptCookie (display,position,top,left,width) {
+ acceptCookie = (display,position,top,left,width) => {
     cookies.style.display = display;
     cookies.style.position = position;
     cookies.style.top = top;
     cookies.style.left = left;
-    cookies.style.width = width;
-      
+    cookies.style.width = width;     
 };
 
 
 main.addEventListener(trigger1, bluredBody('blur(8px)'));
-cookies.addEventListener(trigger1, acceptCookie('block','absolute','30%','10%','80vw'));
+cookies.addEventListener(trigger1, acceptCookie('block','fixed','6%','0%','80vw'));
+//cookies.addEventListener(trigger1, acceptCookie('block','absolute','3%','10%','80vw'));
 
-
-btn.addEventListener(trigger2, function (event) {
+for (let i = 0; i < buttonElement.length; i++) {
+buttonElement[i].addEventListener(trigger2, (event) => {
     bluredBody('blur(0px)');
     acceptCookie('none');
-  });
+  });}
 
   
 
